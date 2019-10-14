72126a5aa96b88cb38321072bf4401425c660b71
michael <michael@foruteam.net>
2019-10-14 16:07:42 +0800

    基本修改

---
+++ src/components/calender.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input @click="testclick" type="button" value="testclick">
+
+    </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        testclick(){
+            console.log('OKOK');
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
